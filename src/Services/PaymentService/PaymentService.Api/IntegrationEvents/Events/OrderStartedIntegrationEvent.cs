5f3dda71553be0e48e3ef443021b1be7c9ad6d5e
Emre Erkoca <emreerkoca@outlook.com>
2022-09-07 00:08:21 +0300

    PaymentService.Api added

---
+++ src/Services/PaymentService/PaymentService.Api/IntegrationEvents/Events/OrderStartedIntegrationEvent.cs
@@ -0,0 +1,19 @@
+﻿using EventBus.Base.Events;
+using System;
+
+namespace PaymentService.Api.IntegrationEvents.Events
+{
+    public class OrderStartedIntegrationEvent : IntegrationEvent
+    {
+        public Guid OrderId { get; set; }
+
+        public OrderStartedIntegrationEvent()
+        {
+        }
+
+        public OrderStartedIntegrationEvent(Guid orderId)
+        {
+            OrderId = orderId;
+        }
+    }
+}
