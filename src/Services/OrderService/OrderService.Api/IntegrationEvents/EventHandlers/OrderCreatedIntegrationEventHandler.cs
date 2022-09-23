f7b16cf0f023364e245df07253ced6c73a573bb9
Emre Erkoca <emreerkoca@outlook.com>
2022-09-23 08:59:58 +0300

    OrderService projects added

---
+++ src/Services/OrderService/OrderService.Api/IntegrationEvents/EventHandlers/OrderCreatedIntegrationEventHandler.cs
@@ -0,0 +1,46 @@
+﻿using EventBus.Base.Abstraction;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using OrderService.Api.IntegrationEvents.Events;
+using OrderService.Application.Features.Commands.CreateOrder;
+using System;
+using System.Threading.Tasks;
+
+namespace OrderService.Api.IntegrationEvents.EventHandlers
+{
+    public class OrderCreatedIntegrationEventHandler : IIntegrationEventHandler<OrderCreatedIntegrationEvent>
+    {
+        private readonly IMediator _mediator;
+        private readonly ILogger<OrderCreatedIntegrationEventHandler> _logger;
+
+        public OrderCreatedIntegrationEventHandler(IMediator mediator, ILogger<OrderCreatedIntegrationEventHandler> logger)
+        {
+            _mediator = mediator;
+            _logger = logger;
+        }
+
+        public async Task Handle(OrderCreatedIntegrationEvent @event)
+        {
+            try
+            {
+                _logger.LogInformation("Handling integration event: {IntegrationEventId} at {AppName} - ({@IntegrationEvent})",
+                @event.Id,
+                typeof(Startup).Namespace,
+                @event);
+
+                var createOrderCommand = new CreateOrderCommand(@event.Basket.Items,
+                                @event.UserId, @event.UserName,
+                                @event.City, @event.Street,
+                                @event.State, @event.Country, @event.ZipCode,
+                                @event.CardNumber, @event.CardHolderName, @event.CardExpiration,
+                                @event.CardSecurityNumber, @event.CardTypeId);
+
+                await _mediator.Send(createOrderCommand);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.ToString());
+            }
+        }
+    }
+}
