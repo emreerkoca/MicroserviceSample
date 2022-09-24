faf091c8ce4f9e838fea65c42d682fb9979dca90
Emre Erkoca <emreerkoca_related@bitbucket.org>
2022-09-24 09:15:45 +0300

    Order service developments

---
+++ src/Services/OrderService/OrderService.Application/IntegrationEvents/OrderStartedIntegrationEvent.cs
@@ -0,0 +1,18 @@
+﻿using EventBus.Base.Events;
+using System;
+
+namespace OrderService.Application.IntegrationEvents
+{
+    public class OrderStartedIntegrationEvent : IntegrationEvent
+    {
+        public string UserName { get; set; }
+
+        public Guid OrderId { get; set; }
+
+        public OrderStartedIntegrationEvent(string userName, Guid orderId)
+        {
+            UserName = userName;
+            OrderId = orderId;
+        }
+    }
+}
