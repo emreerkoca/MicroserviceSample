ccaf39ef81a469fb18e6e736b3d57d4e2e72b65a
Emre Erkoca <emreerkoca_related@bitbucket.org>
2022-09-30 14:02:02 +0300

    CreateOrderCommandHandler added

---
+++ src/Services/OrderService/OrderService.Application/Features/Commands/CreateOrder/CreateOrderCommandHandler.cs
@@ -0,0 +1,45 @@
+﻿using EventBus.Base.Abstraction;
+using MediatR;
+using OrderService.Application.IntegrationEvents;
+using OrderService.Application.Interfaces.Repositories;
+using OrderService.Domain.AggregateModels.OrderAggregate;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OrderService.Application.Features.Commands.CreateOrder
+{
+    public class CreateOrderCommandHandler : IRequestHandler<CreateOrderCommand, bool>
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly IEventBus _eventBus;
+
+
+        public CreateOrderCommandHandler(IOrderRepository orderRepository, IEventBus eventBus)
+        {
+            _orderRepository = orderRepository;
+            _eventBus = eventBus;
+        }
+
+        public async Task<bool> Handle(CreateOrderCommand request, CancellationToken cancellationToken)
+        {
+            var address = new Address(request.Street, request.City, request.State, request.Country, request.ZipCode);
+
+            Order dbOrder = new(request.UserName,
+                address, request.CardTypeId, request.CardNumber, request.CardSecurityNumber, request.CardHolderName, request.CardExpiration, null);
+
+            foreach (var orderItem in request.OrderItems)
+            {
+                dbOrder.AddOrderItem(orderItem.ProductId, orderItem.ProductName, orderItem.UnitPrice, orderItem.PictureUrl, orderItem.Units);
+            }
+
+            await _orderRepository.AddAsync(dbOrder);
+            await _orderRepository.UnitOfWork.SaveEntitiesAsync(cancellationToken);
+
+            var orderStartedIntegrationEvent = new OrderStartedIntegrationEvent(request.UserName, dbOrder.Id);
+
+            _eventBus.Publish(orderStartedIntegrationEvent);
+
+            return true;
+        }
+    }
+}
